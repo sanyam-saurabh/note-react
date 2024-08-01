88d142250c9d7be5459cbbc20bd65dbb2c6c4fae
sanyam-saurabh <saurabhemc2@gmail.com>
2024-08-01 10:53:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e) => {
+    const newVal = e.target.value;
+    setTask(newVal);
+  }
+
+  const addTask = () => {
+    setItems((prev) => {
+      return [...prev, task];
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={task}/>
+        <button onClick={addTask}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => {
+            return <li>{item}</li>
+
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
